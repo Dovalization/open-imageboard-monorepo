4a89f44819b927ba4098b13d009a982313c19985
Guilherme Doval <guilherme.doval@epicgames.com>
2025-04-01 02:51:38 -0300

    fix(api): update import path for AccountsRepository to use absolute path

--- apps/api/src/modules/database/prisma/prisma.module.ts
+++ apps/api/src/modules/database/prisma/prisma.module.ts
@@ -1,6 +1,6 @@
 import { Global, Module } from '@nestjs/common';
 
-import { AccountsRepository } from '../../accounts/accounts-repository';
+import { AccountsRepository } from '@api/modules/accounts/accounts-repository';
 import { PrismaRepository } from './prisma-repository';
 import { PrismaService } from './prisma.service';
 
